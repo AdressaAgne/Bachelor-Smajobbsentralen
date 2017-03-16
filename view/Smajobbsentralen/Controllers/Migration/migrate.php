b02e17bf019d051a3917edc04bdbf9ef8f079c8b
Agne <agne240@me.com>
2017-03-16 14:33:30 +0100

    migration for categories and opening times

--- view/Smajobbsentralen/Controllers/Migration/migrate.php
+++ view/Smajobbsentralen/Controllers/Migration/migrate.php
@@ -6,3 +6,68 @@ $db->createTable('kategorier', [
     new Row('name', 'varchar'),
     new Row('icon', 'varchar'),
 ]);
+
+$db->insert('kategorier', [
+    [
+        'name' => 'snømåking',
+        'icon' => 'user',
+    ],
+    [
+        'name' => 'hagearbeid',
+        'icon' => 'user',
+    ],
+    [
+        'name' => 'kjøring',
+        'icon' => 'user',
+    ],
+    [
+        'name' => 'gressklipping',
+        'icon' => 'user',
+    ],
+    [
+        'name' => 'handling',
+        'icon' => 'user',
+    ],
+    [
+        'name' => 'flytting',
+        'icon' => 'user',
+    ],
+    [
+        'name' => 'maling',
+        'icon' => 'user',
+    ],
+    [
+        'name' => 'møblering',
+        'icon' => 'user',
+    ],
+    [
+        'name' => 'vasking',
+        'icon' => 'user',
+    ],
+    [
+        'name' => 'småarbeid',
+        'icon' => 'user',
+    ],
+    
+]);
+
+$db->createTable('opningstider', [
+    new PID(),
+    new Row('day', 'int'),
+    new Row('from_time', 'varchar'),
+    new Row('to_time', 'varchar'),
+]);
+
+//days starts on sunday = 0;
+$db->insert('opningstider', [
+    [
+        'day' => 2, // tirsdag
+        'from_time' => '10:00',
+        'to_time' => '12:00',
+    ],
+    [
+        'day' => 4, // torsdag
+        'from_time' => '10:00',
+        'to_time' => '12:00',
+    ],
+]);
