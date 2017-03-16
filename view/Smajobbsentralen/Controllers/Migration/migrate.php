212ce8038dbb3a022e39bfffd5352cbd4b1c5c1e
lukteperq <grimstadoyvind@gmail.com>
2017-03-16 20:56:44 +0100

    populate() bug migrate.php

--- view/Smajobbsentralen/Controllers/Migration/migrate.php
+++ view/Smajobbsentralen/Controllers/Migration/migrate.php
@@ -18,6 +18,25 @@ class Migrate {
 			new Row('to_time', 'varchar'),
 		]);
 	}
+	$db->deleteTable('users');
+
+	$db->createTable('users', [
+		new PID(),
+		new Timestamp(),
+		new Row('name', 'varchar'),
+		new Row('surname', 'varchar'),
+		new Row('email', 'varchar'),
+		new Row('password', 'varchar'),
+		new Row('approved', 'tinyint'),
+		new Row('visible', 'tinyint'),
+		new Row('dob', 'date'),
+		new Row('mobile_phone', 'int'),
+		new Row('private_phone', 'int'),
+		new Row('car', 'tinyint'),
+		new Row('hitch', 'tinyint'),
+		new Row('occupation', 'varchar'),
+		new Row('other_info', 'varchar')
+	]);
 
 	public function populate($db){
 		$db->insert('kategorier', [
@@ -94,26 +113,6 @@ class Migrate {
 			]
 		]);
 
-		$db->deleteTable('users');
-
-		$db->createTable('users', [
-			new PID(),
-			new Timestamp(),
-			new Row('name', 'varchar'),
-			new Row('surname', 'varchar'),
-			new Row('email', 'varchar'),
-			new Row('password', 'varchar'),
-			new Row('approved', 'tinyint'),
-			new Row('visible', 'tinyint'),
-			new Row('dob', 'date'),
-			new Row('mobile_phone', 'int'),
-			new Row('private_phone', 'int'),
-			new Row('car', 'tinyint'),
-			new Row('hitch', 'tinyint'),
-			new Row('occupation', 'varchar'),
-			new Row('other_info', 'varchar')
-		]);
-
 		$db->insert('users', [
 			[
 				'name' => 'admin',
@@ -138,76 +137,4 @@ class Migrate {
 			]
 		]);
 	}//populate
-
-
-}
-
-
-
-
-/*
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost:8889
--- Generation Time: 16. Mar, 2017 19:57 PM
--- Server-versjon: 5.6.34
--- PHP Version: 7.0.13
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Database: `smajobb`
---
-
--- --------------------------------------------------------
-
---
--- Tabellstruktur for tabell `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `password` varchar(255) NOT NULL,
-  `cookie` varchar(255) NOT NULL,
-  `image` int(11) NOT NULL DEFAULT '1',
-  `mail` varchar(255) NOT NULL,
-  `visible` tinyint(11) NOT NULL,
-  `approved` tinyint(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `surname` varchar(255) NOT NULL,
-  `mobile` int(11) NOT NULL,
-  `phone` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dataark for tabell `users`
---
-
-INSERT INTO `users` (`id`, `time`, `password`, `cookie`, `image`, `mail`, `visible`, `approved`, `name`, `surname`, `mobile`, `phone`) VALUES
-(1, '2017-03-16 15:47:38', '123', '', 1, 'asd@asd.asd', 1, 1, 'øyvind', 'skjiitsek', 47343090, 47343090),
-(2, '2017-03-16 15:54:28', '123', '', 1, '123@123.cm', 1, 1, 'agnis', 'agnos', 12341234, 12341234);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-*/
+}//class
