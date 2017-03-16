47f130e0b811541edd8dbd674fd2a28ad423f408
lukteperq <grimstadoyvind@gmail.com>
2017-03-16 12:12:17 +0100

    smajobber frontend

--- view/Smajobbsentralen/view/posts/smajobber.php
+++ view/Smajobbsentralen/view/posts/smajobber.php
@@ -1,6 +1,7 @@
-<div class="row">
+<div class="row smajobbere-list">
 	<div class="col-12">
 		<h1>{{$post->header}}</h1>
-		<h1><strong><i class="fa fa-facebook"></i> {{$post->content}}</strong></h1>
+		<h2>Telefon</h2>
+		<h1><strong><i class="fa fa-phone"></i> {{$post->content}}</strong></h1>
 	</div>
 </div>
