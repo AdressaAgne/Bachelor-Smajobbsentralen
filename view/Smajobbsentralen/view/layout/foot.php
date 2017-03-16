b0db03ee259a7af152063b68c8258607cff1ad18
lukteperq <grimstadoyvind@gmail.com>
2017-03-16 14:21:55 +0100

    foot

--- view/Smajobbsentralen/view/layout/foot.php
+++ view/Smajobbsentralen/view/layout/foot.php
@@ -1,4 +1,16 @@
-    </div><!-- /container-fluid -->
-    <script src="{{$assets}}/js/paralex.js"></script>
+	</div><!-- /container-fluid -->
+	<footer>
+		<div class="row">
+			<div class="col-12">
+				<h3>Telefon</h3>
+				<h1 class="vakttlf"><strong><i class="fa fa-phone"></i> 61 10 95 50</strong></h1>
+				<p>Kl: 10:00 - 12:00 Tirsdag og Torsdag</p>
+				<p>Adresse</p>
+				<p>Øvre Torvgate 2</p>
+				<p>2815 Gjøvik</p>
+			</div>
+		</div>
+	</footer>
+	<script src="{{$assets}}/js/paralex.js"></script>
 </body>
 </html>
