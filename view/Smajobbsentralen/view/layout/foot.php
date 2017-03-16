e2c9f612b566e9e89632dca06b32178261333f2a
lukteperq <grimstadoyvind@gmail.com>
2017-03-16 14:53:18 +0100

    footer bs

--- view/Smajobbsentralen/view/layout/foot.php
+++ view/Smajobbsentralen/view/layout/foot.php
@@ -4,8 +4,8 @@
 			<div class="col-12">
 				<h3>Telefon</h3>
 				<h1 class="vakttlf"><strong><i class="fa fa-phone"></i> 61 10 95 50</strong></h1>
-				<p>Kl: 10:00 - 12:00 Tirsdag og Torsdag</p>
-				<p>Adresse</p>
+				<h4>Kl: 10:00 - 12:00 Tirsdag og Torsdag</h4>
+				<h4>Adresse:</h4>
 				<p>Øvre Torvgate 2</p>
 				<p>2815 Gjøvik</p>
 			</div>
