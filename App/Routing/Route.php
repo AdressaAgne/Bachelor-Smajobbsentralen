2f32b70a59f9cd318acef2f6df94eccbda2d5506
Agne <agne240@me.com>
2017-03-22 12:38:42 +0100

    improved CSRF

--- App/Routing/Route.php
+++ App/Routing/Route.php
@@ -33,8 +33,10 @@ class Route {
         
         if($_SERVER['REQUEST_METHOD'] == "POST"){
             //CSRF token
+            if(!isset($_POST['_token'])) return self::error('401', ['missing token'])
+            
             if($_POST['_token'] != $_SESSION['_token']){
-               return self::error('401', ['Missing/Wrong CSRF token']);
+               return self::error('401', ['Wrong CSRF token']);
             } 
 
             switch(strtoupper($_POST['_method'])) {
