b03873a7b34974ccc84d46e48cdfa6691923743b
Agne <agne240@me.com>
2017-03-27 20:58:54 +0200

    footer

--- view/Smajobbsentralen/view/layout/foot.php
+++ view/Smajobbsentralen/view/layout/foot.php
@@ -1,23 +1,17 @@
 	</div><!-- /container-fluid -->
 	<footer>
 		<div class="row">
-			<div class="col-s-12">
-				<h3>Telefon</h3>
-				<h1 class="vakttlf"><strong><i class="fa fa-phone"></i> 61 10 95 50</strong></h1>
-				<h4>Kl: 10:00 - 12:00 Tirsdag og Torsdag</h4><!-- TODO -->
-				<h4>Adresse:</h4>
-				<p>Øvre Torvgate 2</p>
-				<p>2815 Gjøvik</p>
-			</div>
-
-			<div class="row">
-				<div class="col--right footer-links">
-					<a href="/skjiit">Logg inn som telefonvakt</a>
-					<a href="/skjiit">Logg inn som småjobber</a>
+			<div class="container">
+				<div class="col-12">
+					<h3>Telefon</h3>
+					<h1 class="vakttlf"><strong><i class="fa fa-phone"></i> 61 10 95 50</strong></h1>
+					<h4>Kl: 10:00 - 12:00 Tirsdag og Torsdag</h4><!-- TODO -->
+					<h4>Adresse:</h4>
+					<p>Øvre Torvgate 2</p>
+					<p>2815 Gjøvik</p>
 				</div>
 			</div>
 		</div>
 	</footer>
-	<script src="{{$assets}}/js/paralex.js"></script>
 </body>
 </html>
