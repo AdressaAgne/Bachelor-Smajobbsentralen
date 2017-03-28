ab959468793eec9afde6c6df6d9646c2ef6a4eb8
Agne <agne240@me.com>
2017-03-28 14:45:35 +0200

    footer

--- view/Smajobbsentralen/view/layout/foot.php
+++ view/Smajobbsentralen/view/layout/foot.php
@@ -6,9 +6,10 @@
 					<?php $open = $global->opentimes(); ?>
 					<h3>Telefon</h3>
 					<h1 class="vakttlf"><strong><i class="fa fa-phone"></i> 61 10 95 50</strong></h1>
-					<p>{{$open['isOpen']}} nå</p>
+					
+					<h4><strong>Åpningstider:</strong> {{$open['isOpen']}} nå</h4>
 					@foreach($open['timeStr'] as $str)
-                        <p class="font-small">{{ $str }}</p>
+                        <p>{{ $str }}</p>
                     @endforeach
 					<h4>Adresse:</h4>
 					<p>Øvre Torvgate 2</p>
