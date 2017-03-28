e65ec0bc868bdb01e165c232ebfaceb7c357228c
lukteperq <grimstadoyvind@gmail.com>
2017-03-28 21:38:47 +0200

    telefonvakt framside

--- view/Smajobbsentralen/view/pages/telefonvakt.php
+++ view/Smajobbsentralen/view/pages/telefonvakt.php
@@ -7,3 +7,92 @@
 	@endif
 </div>
 @layout('layout.telefonvakt_menu')
+<!-- TODO loop igjennom fra backend. Evt vurder table istedet -->
+<div class="row">
+	<div class="col-6">
+		<h3>Mars</h3>
+		<div class="col-12 dagsOversikt">
+			<h2>Tirsdag</h2>
+
+			<div class="row dag">
+				<div class="col-1 col-m-12">
+					<p>28</p>
+				</div>
+				<div class="col-9 col-m-12 midt">
+					<p>heisann snerkefjes</p>
+				</div>
+				<div class="col-2 col-m-12">
+					<button class="btn">info</button>
+				</div>
+			</div>
+
+			<div class="row dag">
+				<div class="col-1 col-m-12">
+					<p>28</p>
+				</div>
+				<div class="col-9 col-m-12 midt">
+					<p>heisann snerkefjes</p>
+				</div>
+				<div class="col-2 col-m-12">
+					<button class="btn">info</button>
+				</div>
+			</div>
+
+			<div class="row dag">
+				<div class="col-1 col-m-12">
+					<p>28</p>
+				</div>
+				<div class="col-9 col-m-12 midt">
+					<p>heisann snerkefjes</p>
+				</div>
+				<div class="col-2 col-m-12">
+					<button class="btn">info</button>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<div class="col-6">
+		<h3>April</h3>
+		<div class="col-12 dagsOversikt">
+			<h2>Torsdag</h2>
+
+			<div class="row dag">
+				<div class="col-1 col-m-12">
+					<p>2</p>
+				</div>
+				<div class="col-9 col-m-12 midt">
+					<p>heisann</p>
+				</div>
+				<div class="col-2 col-m-12">
+					<button class="btn">info</button>
+				</div>
+			</div>
+
+			<div class="row dag">
+				<div class="col-1 col-m-12">
+					<p>2</p>
+				</div>
+				<div class="col-9 col-m-12 midt">
+					<p>heisann</p>
+				</div>
+				<div class="col-2 col-m-12">
+					<button class="btn">info</button>
+				</div>
+			</div>
+
+			<div class="row dag">
+				<div class="col-1 col-m-12">
+					<p>2</p>
+				</div>
+				<div class="col-9 col-m-12 midt">
+					<p>heisann</p>
+				</div>
+				<div class="col-2 col-m-12">
+					<button class="btn">info</button>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
