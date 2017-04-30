1540145f66b1ade813513325b9a25276f2ba997c
Agne <agne240@me.com>
2017-04-30 02:28:50 +0200

    fixed menu

--- public/view/layout/menu.php
+++ public/view/layout/menu.php
@@ -4,10 +4,14 @@
 		<li class="nav__item"><a href="{{$source}}/">Hjem</a></li><!-- legg inn logo TODO -->
 		<li class="nav__item"><a href="{{$source}}/smajobbere">Småjobbere</a></li>
 		<li class="nav__item"><a href="{{$source}}/blismajobber">Bli Småjobbere</a></li>
-		<li class="nav__item"><a href="{{$source}}/admin">Admin</a></li>
 	</ul>
 	<ul class="nav--right">
-		<li class="nav__item"><a href=""><span class="liten">A</span>-A</a></li>
+        @if(isset($user))
+		    <li class="nav__item"><a href="{{$source}}/admin">Admin</a></li>
+        @else    
+            <li class="nav__item"><a href="{{$source}}/admin">Login</a></li>
+        @endif
+		<!-- <li class="nav__item"><a href=""><span class="liten">A</span>-A</a></li> -->
 	</ul>
   </div>
 </nav>
